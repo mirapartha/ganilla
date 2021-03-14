cc2900cf0611558968c174f674b76ea0a98eaea2
Mira Partha <mirapartha@gmail.com>
2021-03-13 16:54:33 -0800

    fixed wrong commented line

--- util/visualizer.py
+++ util/visualizer.py
@@ -38,8 +38,8 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256, f_nam
             image_name = os.path.splitext(f_name)[0] + ".png" # cityscape icin eklendi
         else:
             image_name = '%s.png' % (name) # image_name = '%s_%s.png' % (name, label)
-        #save_path = os.path.join(os.path.join(image_dir,'..\..\..'), image_name) #os.path.join(image_dir, image_name)
-        print(save_path)
+        save_path = os.path.join(os.path.join(image_dir,'..\..\..'), image_name) #os.path.join(image_dir, image_name)
+        #print(save_path)
         h, w, _ = im.shape
         if aspect_ratio > 1.0:
             im = np.array(Image.fromarray(obj=im).resize(size=(h, int(w * aspect_ratio)), resample=Image.BICUBIC)) # im = imresize(im, (h, int(w * aspect_ratio)), interp='bicubic')
